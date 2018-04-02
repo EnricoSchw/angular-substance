ee10f6898ece7e4b0861def96fa3bd63d628ff17
EnricoSchw <enrico.schw@gmx.de>
2018-04-02 23:02:20 +0200

    Init substance module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AngularSubstanceModule } from './modules/angular-substance/angular-substance.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularSubstanceModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/modules/angular-substance/angular-substance.module.ts
+++ src/app/modules/angular-substance/angular-substance.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularSubstanceComponent } from './angular-substance.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        CommonModule
+    ],
+    declarations: [AngularSubstanceComponent],
+    exports: [AngularSubstanceComponent]
 })
 export class AngularSubstanceModule {
 }
